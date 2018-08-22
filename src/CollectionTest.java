b8bd2b5c2afb4e059c637930bef76cea602aac7f
Parjinder Pannu <ppannu@cisco.com>
2018-08-21 17:33:02 -0700

    Tried CollectionTest

---
+++ src/CollectionTest.java
@@ -0,0 +1,49 @@
+// Fig 16.2 CollectionTest.java
+// Collection interface demonstrated via an ArrayList object
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+
+public class CollectionTest {
+    public static void main(String[] args){
+//        add elements in colors array to list
+        String[] colors = {"MAGENTA", "RED", "WHITE", "BLUE", "CYAN"};
+        List<String> list = new ArrayList<String>();
+
+        for (String color : colors)
+            list.add(color); // adds color to end of list
+
+        // add elements in removeColors array to removeList
+        String[] removeColors = {"RED", "WHITE", "BLUE"};
+        List<String> removeList = new ArrayList<String>();
+
+        for(String color : removeColors)
+            removeList.add(color);
+
+        System.out.println("ArrayList: ");
+
+        for(int count = 0; count < list.size(); count++) {
+            System.out.printf("%s ", list.get(count));
+        }
+
+        removeColors(list, removeList);
+
+        System.out.printf("%n%nArrayList after calling removeColors:%n");
+
+        for(String color : list)
+            System.out.printf("%s ", color);
+    }
+
+    private static void removeColors(Collection<String> collection1, Collection<String> collection2)
+    {
+        Iterator<String> iterator = collection1.iterator();
+
+        while (iterator.hasNext())
+        {
+            if(collection2.contains((iterator.next())))
+                iterator.remove();
+        }
+    }
+}
